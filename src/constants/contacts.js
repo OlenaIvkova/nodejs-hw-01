085708a83b994812e847529713ecd9fe06ce3a91
Olena <alchimeraa@gmail.com>
2025-01-14 00:39:37 +0200

    src

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB =
+// export const PATH_DB = '../db/db.json';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export const PATH_DB = path.resolve(__dirname, '../db/db.json');
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+// import { promises as fs } from 'fs';
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+  }
+};
